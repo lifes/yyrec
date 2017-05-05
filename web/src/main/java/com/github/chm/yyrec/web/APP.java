82d3f023c17cfae6d2b1d51ecd3923d6eedd2d35
PeterC01 <tt@h.com>
2017-05-05 16:35:55 +0800

    .

---
+++ web/src/main/java/com/github/chm/yyrec/web/APP.java
@@ -0,0 +1,11 @@
+package com.github.chm.yyrec.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APP {
+	public static void main(String args[]) {
+		SpringApplication.run(APP.class, args);
+	}
+}
